afea9b0d159004f8ce29642c41c860ee2226ed96
luavipez <luavipez@gmail.com>
2025-08-23 22:20:41 -0600

    Commit backend

---
+++ backend/users/url.py
@@ -0,0 +1,7 @@
+# backend/urls.py
+from django.contrib import admin
+from django.urls import path, include
+urlpatterns = [
+ path('admin/', admin.site.urls),
+ path('api/', include('users.urls')),
+ ]
