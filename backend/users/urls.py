866d76d1ef18d4578d81678ca1c0f126534d15cd
luavipez <luavipez@gmail.com>
2025-08-25 00:03:42 -0600

    Formulario de login en React funcionando

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-     path('user/*', views.User, name='register'),
+     path('login/*', views.User, name='register'),
 ]
